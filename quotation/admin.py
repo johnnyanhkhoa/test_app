c546845e3993a19d651fcdd434da9d58a292b34e
johnnyanhkhoa <johnnyanhkhoa@gmail.com>
2022-08-24 22:22:42 +0700

    s

--- quotation/admin.py
+++ quotation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_header = "Tedis - Sale Agreement"
 # Register your models here.
 admin.site.register(Quotation)
 admin.site.register(Quotation_product)
